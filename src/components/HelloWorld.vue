a0c349c6af7e3a71bfb1be7dcf9fe404af3f60b4
ronaldoFlores3552 <ronaldo.flores@utec.edu.pe>
2024-04-08 21:45:46 -0500

    primer avanze

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -97,6 +97,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+/*estos estilos solo se aplican en HelloWorld.*/
 h3 {
   margin: 40px 0 0;
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css"; //el @ se refiere a la raiz
 
+//apra obtener algunos tips de producción
 Vue.config.productionTip = false;
 
+//instanciamos Vue
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
